eeb17da2f507b7ac9f6fb89e747f04f67a20287b
parkjeonghunii <parkjung3145@gmail.com>
2021-05-13 13:44:26 +0900

    ui
    + 10:(16, 17, 18, ...) 범위의 해상도 대응(❗ 가로 해상도 대응이 힘들고, 세로 비율이 10:16보다 더 커야 함))

---
+++ Assets/Scripts/ResizedResolution.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResizedResolution : MonoBehaviour
+{
+    public int gap = 267; // 상단에 플레이어 상태 판넬 높이: 188, 하단에 메뉴 탭 높이: 79
+    private float virtualRatio;
+    private float realRatio;
+    private float changeRatio;
+
+    private float height;
+
+    public List<RectTransform> resizePanel;
+        void Start()
+    {
+        virtualRatio = 1600f / 1000f;
+        realRatio = (float)Screen.height / (float)Screen.width;
+
+        changeRatio = realRatio / virtualRatio;
+
+        height = 1600f * changeRatio;
+        Debug.Log(height);
+        foreach (RectTransform rect in resizePanel)
+            rect.sizeDelta = new Vector2(1000, height-gap);
+    }
+}
