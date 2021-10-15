03c0d83a1b22a9907e05bda9ce57264bf4edd9a8
manduk05 <80938934+manduk05@users.noreply.github.com>
2021-10-16 00:13:17 +0900

    스테이지 통합
    스테이지를 통합하고 장애물 시작과 끝을 랜덤으로 지정하여 조금 다채롭게 진행되게 하였습니다

--- Assets/2.Scripts/AreaSpawner.cs
+++ Assets/2.Scripts/AreaSpawner.cs
@@ -27,19 +27,25 @@ public class AreaSpawner : MonoBehaviour
     [SerializeField]
     private Transform playerTransform;
     public int clear;
-    private int whichSpawn;
     public int stage;
     Rigidbody indirigi;
     Rigidbody torigi;
     GameObject Player;
-
+    private int obsStart;
+    private int obsEND;
+    private int E_len;
+    private int O_len;
 
     private void Awake()
     {
         stage = 1;
         clear = 0;
-        //whichSpawn = Random.Range(0, 2);     //몬스터 or 장애물 스테이지 랜덤으로 선택
-        whichSpawn = 0;     //몬스터 생성 알고리즘 테스트용도
+
+        obsStart = Random.Range(10, 25);     //장애물 스폰 시작점
+        obsEND = Random.Range(obsStart + 3, 47);        //끝점
+        E_len = enemies.Length;
+        O_len = obstacles.Length;
+
 
         for (int i = 0; i < spawnAreaCountAtStart; ++i)
         {
@@ -67,82 +73,75 @@ public class AreaSpawner : MonoBehaviour
         areaIndex++;
 
         clear++; // 스테이지 클리어 조건
-
     }
     public void Spawn()
     {
-        //MiddleBoss middle;
+        int isSpawn;
 
-        int isSpawn, len;
-        len = enemies.Length;
 
-        if (clear >= 50)       //clear 변수가 일정수준 도달했을 경우 패널을 띄운 뒤 변수 초기화
+        Debug.Log("obsStart=" + obsStart);
+        Debug.Log("obsEND=" + obsEND);
+
+        if (clear >= 60)       //clear 변수가 일정수준 도달했을 경우 패널을 띄운 뒤 변수 초기화
         {
             Debug.Log("clear!");
             Time.timeScale = 0;
             panelController.OpenPanel(3);
             clear = 0;
-            whichSpawn = Random.Range(0, 2); //다음 스테이지 몬스터 or 장애물 스테이지 랜덤으로 선택
+            obsStart = Random.Range(10, 25);     //장애물 스폰 시작점
+            obsEND = Random.Range(obsStart + 3, 47);        //끝점
         }
-        if (clear >=10&&clear<=45)
-        {
-            float X_coord = -1.5f;        //몬스터 X좌표
 
-            for (int i=0;i<3;i++)
+
+        if ( clear>= obsStart&& clear<= obsEND)
+        {
+            float X_coord = -1.5f;        //장애물 X좌표
+            for (int i = 0; i < 3; i++)
             {
                 isSpawn = Random.Range(0, 10);       //스폰 할 것 인지 말 것인지 (70%)
                 if (isSpawn > 2)
-                    SpawnMonster(len, X_coord);
+                    SpawnObstacle(O_len, X_coord);
                 X_coord += 1.5f;
             }
-
         }
-        if (clear <45 && clear %10==0)       // clear 조건에 따라 토네이도 이벤트 발생
+        else if (clear<=55 && clear>=8)
+        {
+            float X_coord = -1.5f;        //몬스터 X좌표
+
+            for (int i = 0; i < 3; i++)
+            {
+                isSpawn = Random.Range(0, 10);       //스폰 할 것 인지 말 것인지 (70%)
+                if (isSpawn > 2)
+                    SpawnMonster(E_len, X_coord);
+                X_coord += 1.5f;
+            }
+        }
+
+
+        if (clear <55 && clear %10==0)       // clear 조건에 따라 토네이도 이벤트 발생
         {
             int loc;
 
             loc = Random.Range(0, 3);       //토네이도 위치를 위한 랜덤변수
             if (loc == 0)       //좌측
             {
-                StartCoroutine(Tornado(-1f, 0f, playerTransform.position.z, 84f));
+                StartCoroutine(Tornado(-1f, 1f, playerTransform.position.z, 84f));
             }
             else if (loc == 1)        //중간
             {
-                StartCoroutine(Tornado(0f, 0f, playerTransform.position.z, 90f));
+                StartCoroutine(Tornado(0f, 1f, playerTransform.position.z, 90f));
             }
             else           //우측
             {
-                StartCoroutine(Tornado(1f, 0f, playerTransform.position.z, 96f));
+                StartCoroutine(Tornado(1f, 1f, playerTransform.position.z, 96f));
             } 
         }
-        //if (clear < 23 && clear % 5 == 0)       
-        //{
-        //    int loc;
-
-        //    loc = Random.Range(0, 3);
-        //    if (loc == 0)       //좌측
-        //    {
-        //        Vector3 enem_transform = new Vector3(-1.5f, 0, playerTransform.position.z + 30);    //이벤트 좌표값
-        //        Instantiate(events[1], enem_transform, transform.rotation));   //rotation
-        //    }
-
-        //}
-
-
-
-        /* else if (clear == 23)              //일정 타일 생성 후 중간보스 스폰
-         {
-             Vector3 enem_transform = new Vector3(0, 0, playerTransform.position.z + 15);
-             middle = Instantiate(middleboss, enem_transform, transform.rotation = Quaternion.Euler(new Vector3(0, 180, 0)));
-             clear++;
-         }*/
     }
 
     public void SpawnMonster(int len, float X_coord)
     {
-        int index,y;
+        int index;
         index = 0;
-        y = 0;
 
         index = Random.Range(0, len);       //각 몬스터 스폰 확률 (0=range, 1=burrow, 2=rush , 3=sneak)
  
@@ -151,75 +150,16 @@ public class AreaSpawner : MonoBehaviour
         Instantiate(enemies[index], enem_transform, transform.rotation = Quaternion.Euler(new Vector3(0, 180, 0)));    //1st몬스터 생성, 마지막은 몬스터 회전
     }
 
-    public void SpawnObstacle()
+    public void SpawnObstacle(int len, float X_coord)
     {
-        int isSpawn, index,len;
+        int index;
         index = 0;
-        len = obstacles.Length;
-        if (clear == 45)       //clear 변수가 일정수준 도달했을 경우 패널을 띄운 뒤 변수 초기화
-        {
-            Time.timeScale = 0;
-            panelController.OpenPanel(3);
-            clear = 0;
-            whichSpawn = Random.Range(0, 2); //다음 스테이지 몬스터 or 장애물 스테이지 랜덤으로 선택
 
-        }
-        if (clear >= 10 && clear <= 43)
-        {
-            float X_coord = -1.5f;        //장애물 X좌표
-            Vector3 obs_transform = new Vector3(X_coord, obstacles[index].transform.position.y, playerTransform.position.z + 40);    //장애물의 좌표값
+        index = Random.Range(0, len);       //장애물 스폰 결정
 
-            isSpawn = Random.Range(0, 10);       //스폰 할 것 인지 말 것인지 (70%)
-            if (isSpawn > 2)
-            {
-                index = Random.Range(0, len); //랜덤 장애물 스폰
+        Vector3 enem_transform = new Vector3(X_coord, obstacles[index].transform.position.y, playerTransform.position.z + 40 + Random.Range(-5.0f, 20.0f));    //장애물 좌표값
 
-                Instantiate(obstacles[index], obs_transform, transform.rotation = Quaternion.Euler(new Vector3(0, 180, 0)));    //1st장애물 생성
-                X_coord += 1.5f;        //2번째 장애물의 X값 변경
-                obs_transform = new Vector3(X_coord, obstacles[index].transform.position.y, playerTransform.position.z + 40);    //장애물 좌표값 변경
-            }
-
-            isSpawn = Random.Range(0, 10);
-            if (isSpawn > 2)
-            {
-                index = Random.Range(0, len);    //2nd 장애물 랜덤 지정
-                Instantiate(obstacles[index], obs_transform, transform.rotation = Quaternion.Euler(new Vector3(0, 180, 0)));    //장애물 생성
-                X_coord += 1.5f;        //3번째 장애물 위한 X값 변경
-                obs_transform = new Vector3(X_coord, obstacles[index].transform.position.y, playerTransform.position.z + 40);    //장애물 좌표값 변경
-
-            }
-
-            isSpawn = Random.Range(0, 10);
-            if (isSpawn > 2)
-            {
-                index = Random.Range(0, len);    //3rd 장애물 랜덤 지정
-                Instantiate(obstacles[index], obs_transform, transform.rotation = Quaternion.Euler(new Vector3(0, 180, 0)));    //3rd장애물 생성
-            }
-
-        }
-        if (clear < 23 && clear % 10 == 0)       // clear 조건에 따라 토네이도 이벤트 발생
-        {
-            int loc;
-
-            loc = Random.Range(0, 3);       //토네이도 위치를 위한 랜덤변수
-            if (loc == 0)       //좌측
-            {
-                StartCoroutine(Tornado(-1.5f,0, playerTransform.position.z,84f));
-            }
-            else if (loc == 1)        //중간
-            {
-                StartCoroutine(Tornado(0f, 0, playerTransform.position.z, 90f));
-            }
-            else           //우측
-            {
-                StartCoroutine(Tornado(1.5f, 0, playerTransform.position.z, 96f));
-            }
-        }
-        else if (clear < 45 && clear % 7 == 0)  // clear 조건에 따라 나무 이벤트 발생
-        {
-            Vector3 enem_transform = new Vector3(-2f, 0, playerTransform.position.z + 30);    //이벤트 좌표값
-            Instantiate(events[1], enem_transform + new Vector3(0, 1f, 0), transform.rotation);
-        }
+        Instantiate(obstacles[index], enem_transform, transform.rotation = Quaternion.Euler(new Vector3(0, 180, 0)));    //장애물터 생성
     }
 
     IEnumerator Tornado(float x, float y, float z, float rot)
@@ -231,7 +171,7 @@ public class AreaSpawner : MonoBehaviour
         indirigi = skill_indicator.GetComponent<Rigidbody>();
         indirigi.velocity = transform.forward * 20;
         Quaternion indirot = skill_indicator.transform.rotation * Quaternion.Euler(new Vector3(0,90,0));
-        //Destroy(skill_indicator, 1.5f);
+        Destroy(skill_indicator, 1.5f);
 
         yield return new WaitForSeconds(1.5f);
 
