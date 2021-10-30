5a5a16b35f6e9500f4221b48ae5f0c6a6c2121a4
parkjeonghunii <parkjung3145@gmail.com>
2021-10-31 05:42:11 +0900

    ui
    -+ 유저 정보현황 최고 스테이지, 처치한 몬스터 수 추가, 배치 변경
    + 유저 정보현황 데이터 동기화(몬스터 수 제외)
    -+ 스테이지 결과창 수정

---
+++ Assets/2.Scripts/UI/UserInfoPanel.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UserInfoPanel : MonoBehaviour
+{
+    public TextMeshProUGUI nickname;
+    public TextMeshProUGUI level;
+    public TextMeshProUGUI exp;
+    public TextMeshProUGUI highStage;
+    public TextMeshProUGUI highScore;
+
+    private DataManager data;
+
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        data = GameObject.Find("DataManager").GetComponent<DataManager>();
+
+        nickname.text = data.getName();
+        level.text = data.getLevel().ToString();
+        highStage.text = data.getHighScore().ToString();
+        highScore.text = data.getHighScore().ToString();
+        exp.text = data.player.current_exp.ToString() + " / " + data.player.next_exp.ToString();
+
+    }
+
+}
